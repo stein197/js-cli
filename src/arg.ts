1e77f4a285c2f475e06c2f226188c7d6365574c1
Nail Gafarov <nil20122013@gmail.com>
2023-08-02 09:56:20 +0500

    Added more docs, refactored reduce() and parse()

--- src/arg.ts
+++ src/arg.ts
@@ -7,30 +7,35 @@ const REGEX_DASH_START = /^-+/;
 /**
  * Parse the given string / array of strings as an arguments list into a structure of arguments and options. The
  * function parses the data in the following way:
- * - If it's a string without leading dashes, then it's considered as an argument (i.e. "param1", "param2")
- * - If it's a string with a single leading dash, then it's considered as a shorthand option (i.e. "-abc" as a "-a",
- *   "-b" and "-c")
+ * - If it's a string without leading dashes and the previous argument doesn't start with a dash, then it's considered
+ *   as an argument (i.e. "arg1", "arg2" is `["arg1", "arg2"]`)
+ * - If it's a string with a single leading dash, then it's considered as a flag array (i.e. "-abc" is `{a: true,
+ *   b: true, c: true}`).
+ * - If it's a single char with a single leading dash, then it's considered as a flag. If the next argument doesn't
+ *   start with a dash, then the next argument is a value of the flag (i.e. "-a" is a `{a: true}`, "-a b" and "-a=b" is
+ *   `{a: "b"}`)
  * - If it's a string with a double leading dash, then it's considered as a single option with optional value that
- *   goes after the option (i.e. "--user", "--user name", "--user=name")
+ *   goes after the option (i.e. "--user" is `{user: true}`, "--user name" is `{user: "name"}`, "--user=name" is
+ *   `{user: "name"}`)
  * - If it's a "--" string, then all next strings are considered as arguments
  * @param args Arguments to parse.
  * @returns Parsed arguments.
  * @example
  * Parsing a string
  * ```ts
- * const data = parse("abc -d val1 -e=val2 -fg val3 --opt1 --opt2 val2 --opt3=val3 -- --opt4");
+ * const data = parse("abc -d val1 -e=val2 -fg def --opt1 --opt2 val2 --opt3=val3 --opt4 \"a b c\" -- --opt5");
  * data == {
- * 	args: ["abc", "val3", "--opt4"],
- * 	opts: {d: "val1", e: "val2", f: true, g: true, opt1: true, opt2: "val2", opt3: "val3"}
+ * 	args: ["abc", "def", "--opt5"],
+ * 	opts: {d: "val1", e: "val2", f: true, g: true, opt1: true, opt2: "val2", opt3: "val3", opt4: "a b c"}
  * }
  * ```
  * @example
  * Parsing an array
  * ```ts
- * const data = parse(["abc", "-d", "val1", "-e=val2", "-fg", "val3", "--opt1", "--opt2", "val2", "--opt3=val3", "--", "--opt4"]);
+ * const data = parse(["abc", "-d", "val1", "-e=val2", "-fg", "def", "--opt1", "--opt2", "val2", "--opt3=val3", "--opt4=\"a b c\"", "--", "--opt5"]);
  * data == {
- * 	args: ["abc", "val3", "--opt4"],
- * 	opts: {d: "val1", e: "val2", f: true, g: true, opt1: true, opt2: "val2", opt3: "val3"}
+ * 	args: ["abc", "def", "--opt5"],
+ * 	opts: {d: "val1", e: "val2", f: true, g: true, opt1: true, opt2: "val2", opt3: "val3", opt4: "a b c"}
  * }
  * ```
  * @example
@@ -53,9 +58,11 @@ export function parse<T extends string>(args: string | string[]): ArgsInfo<T> {
 			wasDoubleDash = true;
 		else if (wasDoubleDash)
 			result.args.push(arg);
-		else if (arg.startsWith(DASH_SINGLE))
+		else if (arg.startsWith(DASH_DOUBLE))
 			result.opts[arg.replace(REGEX_DASH_START, "") as T] = true;
-		else if (!arg.startsWith(DASH_SINGLE) && prevArg.startsWith(DASH_SINGLE))
+		else if (arg.startsWith(DASH_SINGLE))
+			arg.replace(REGEX_DASH_START, "").split("").forEach(char => result.opts[char as T] = true);
+		else if (!arg.startsWith(DASH_SINGLE) && (prevArg.startsWith(DASH_DOUBLE) || prevArg.startsWith(DASH_SINGLE) && prevArg.length === 2))
 			result.opts[prevArg.replace(REGEX_DASH_START, "") as T] = arg;
 		else
 			result.args.push(arg);
@@ -97,26 +104,17 @@ function split(args: string): string[] {
 		prevChar = char;
 	}
 	if (curArg)
-		result.push(curArg)
+		result.push(curArg);
 	return result;
 }
 
 function reduce(prev: string[], cur: string): string[] {
 	if (cur.startsWith(DASH_SINGLE)) {
-		const isDoubleDash = cur.startsWith(DASH_DOUBLE);
 		const [key, ...values] = cur.split(CHAR_EQUAL);
 		const value = unquoteString(values.join(CHAR_EQUAL));
-		if (isDoubleDash) {
-			prev.push(key);
-			if (value)
-				prev.push(value);
-		} else {
-			const [firstOpt, ...restOpts] = key.replace(DASH_SINGLE, "").split("").map(char => DASH_SINGLE + char);
-			prev.push(firstOpt);
-			if (value)
-				prev.push(value);
-			prev.push(...restOpts);
-		}
+		prev.push(key);
+		if (value)
+			prev.push(value);
 	} else {
 		prev.push(cur);
 	}
